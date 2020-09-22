edc44561f2ea4002cd94660ff46619610358ce1b
abhimishr <mishra.abhijeet28@gmail.com>
2020-09-22 16:16:47 +0200

    Dummy commit

--- modules/40_transport/dynamic/traveldistance.R
+++ modules/40_transport/dynamic/traveldistance.R
@@ -1,13 +1,3 @@
-*** |  (C) 2008-2020 Potsdam Institute for Climate Impact Research (PIK)
-*** |  authors, and contributors see CITATION.cff file. This file is part
-*** |  of MAgPIE and licensed under AGPL-3.0-or-later. Under Section 7 of
-*** |  AGPL-3.0, you are granted additional permissions described in the
-*** |  MAgPIE License Exception, version 1.0 (see LICENSE file).
-*** |  Contact: magpie@pik-potsdam.de
-
-pc40_distance(j) = f40_distance(j);
-
-$onecho > traveldistance.R
 .getYear <- function(gdx="traveldistanceOut.gdx") {
   return(as.character(gdx::readGDX(gdx,"ct")))
 }
@@ -126,4 +116,3 @@ prediction    <- .predictaccessibility(land_hr, mfile)
 .saveprediction(prediction,year,prediction_file)
 prediction_lr <- .aggregateprediction(prediction$enhanced,land_hr)
 .prediction2gdx(prediction_lr, gdxin, gdxout)
-$offecho
